1db584a6133d02c5e0dce22c2c5a068a88ee2a82
Steven Moder <java20131114@gmail.com>
2024-06-15 23:50:04 +0800

    chore: Update NotFoundPage.vue to use pink color for 404 text

--- web/src/pages/NotFoundPage.vue
+++ web/src/pages/NotFoundPage.vue
@@ -2,7 +2,7 @@
   <section class="bg-gray-200 dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl h-screen lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
-        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-pink-600 dark:text-pink-500">
           404
         </h1>
         <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
@@ -13,7 +13,7 @@
           home page.
         </p>
         <router-link
-          class="inline-flex text-white bg-pink-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+          class="inline-flex text-white bg-pink-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-pink-900 my-4"
           to="/">
           Back to Homepage
         </router-link>
