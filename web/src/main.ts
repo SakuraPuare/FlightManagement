2ecc59b9da6180356a350f653abe88c51e9942ef
Steven Moder <java20131114@gmail.com>
2024-06-18 02:39:13 +0800

    refactor: Remove unused ElementPlus import

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,7 +5,6 @@ import App from "./App.vue";
 import router from "./utils/router.ts";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import "element-plus/dist/index.css";
-import ElementPlus from "element-plus";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -13,6 +12,5 @@ pinia.use(piniaPluginPersistedstate);
 
 app.use(router);
 app.use(pinia);
-app.use(ElementPlus);
 
 app.mount("#app");
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -23,4 +23,19 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
